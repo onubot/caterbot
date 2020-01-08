3b15a926b9f243d78c865bb5ffe190c83f604936
manashmandal <manashmndl@gmail.com>
2020-01-08 15:08:46 +0600

    fix

--- src/messenger/facebook_messenger.py
+++ src/messenger/facebook_messenger.py
@@ -1,10 +1,16 @@
 from fbmessenger import BaseMessenger, MessengerClient
+from fbmessenger.elements import Element
+from fbmessenger import elements
+from fbmessenger import templates
+from fbmessenger.templates import ListTemplate
 import os
 from pprint import pprint
 from src.db import mongodb
 from src.states import State
 import datetime
 from src.messenger.flows import register, index_user, login, update_state
+from src.messenger import food_item_list
+
 
 PAGE_ID = "619830321739930"
 
@@ -23,6 +29,8 @@ class Messenger(BaseMessenger):
         if sender_id != PAGE_ID:
             index_user.update(message)
             user = index_user.get(sender_id)
+
+            print(index_user.get(sender_id))
 
             if (
                 user["current_state"] == State.LOGIN
@@ -46,12 +54,23 @@ class Messenger(BaseMessenger):
                     )
                     self.send(
                         {
-                            "text": "অভিনন্দন, আপনি সিস্টেমে লগিন করতে পেরেছেন, আপনি চাইলে আগামীকালের জন্য এখনি অর্ডার করতে পারেন"
+                            "text": f"অভিনন্দন {username}, আপনি সিস্টেমে লগিন করতে পেরেছেন, আপনি চাইলে আগামীকালের জন্য এখনি অর্ডার করতে পারেন"
                         }
                     )
 
                 else:
-                    self.send({})
+                    self.send(
+                        {
+                            "text": "দুঃখিত, আপনার ইউজারনেম বা পাসওয়ার্ডটি ভুল। দয়া করে আবার ট্রাই করুন।"
+                        }
+                    )
+
+            else:
+                self.send(
+                    {
+                        "text": "আপনি লগডইন অবস্থায়ই আছেন, চাইলে আগামীকালের জন্য অর্ডার প্লেস করুন। ধন্যবাদ।"
+                    }
+                )
 
             print(message)
 
@@ -65,8 +84,16 @@ class Messenger(BaseMessenger):
         pass
 
     def postback(self, message):
-        # mongodb.cx.caterbot.user.update_one()
         print("postback", message)
+
+        if message["postback"]["payload"] == State.SCHEDULE_ORDER:
+            self.send(
+                templates.GenericTemplate(
+                    elements=food_item_list.food_elements,
+                ).to_dict(),
+                "RESPONSE",
+            )
+
 
         if message["postback"]["payload"] == State.REGISTER:
             pass
@@ -77,17 +104,27 @@ class Messenger(BaseMessenger):
 
         if message["postback"]["payload"] == State.LOGIN:
             if message["sender"]["id"] != PAGE_ID:
-                update_state.update(
-                    message["sender"]["id"],
-                    current_state=State.LOGIN,
-                    state_completion=False,
-                )
-                self.send(
-                    {
-                        "text": "দয়া করে ইউজারনেম ও পাসওয়ার্ড দিন, উদাহরণ: username password"
-                    },
-                    "RESPONSE",
-                )
+                user = index_user.get(message["sender"]["id"])
+
+                if user["logged_in"]:
+                    self.send(
+                        {
+                            "text": "আপনি লগড-ইন অবস্থায়ই আছেন, চাইলে আগামীকালের জন্য অর্ডার প্লেস করুন। ধন্যবাদ।"
+                        }
+                    )
+
+                else:
+                    update_state.update(
+                        message["sender"]["id"],
+                        current_state=State.LOGIN,
+                        state_completion=False,
+                    )
+                    self.send(
+                        {
+                            "text": "দয়া করে ইউজারনেম ও পাসওয়ার্ড দিন, উদাহরণ: username password"
+                        },
+                        "RESPONSE",
+                    )
 
     def optin(self, message):
         pass
---
+++ src/messenger/food_item_list.py
@@ -0,0 +1,21 @@
+from fbmessenger.templates import ListTemplate
+from fbmessenger.elements import Element, Button
+import json
+import os
+
+food_list = json.load(open(os.path.join(os.getcwd(), "food_sample.json"), "r"))
+
+food_elements = [
+    Element(
+        title=item["title"],
+        item_url="https://example.com",
+        image_url=item["image_url"],
+        subtitle=item["subtitle"],
+        buttons=[
+            Button(
+                button_type="postback", title=f"অর্ডার করুন", payload=f"ORDER#{item["food_id"]}",
+            )
+        ],
+    )
+    for item in food_list["foods"])
+]
