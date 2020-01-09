7489faca40e4cb2823b6d4717f6e9f4820f39a58
manashmandal <manashmndl@gmail.com>
2020-01-09 16:39:17 +0600

    some fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 git log -1 > commit.log
-killall gunicorn
-/home/ubuntu/caterbot/venv/bin/gunicorn /home/ubuntu/caterbot/app:app -b 0.0.0.0:5000 -D
+/home/ubuntu/caterbot/venv/bin/gunicorn app:app -b 0.0.0.0:5000 -D
